8c06222f7d7b4ee00de6c7ee7642e1853ca8e0ff
Stephen Oberholtzer <stevie@qrpff.net>
2013-11-15 22:12:28 -0500

    sketch_nov15a: Add test mode to facilitate mapping pin/segment relationship

--- sketch_nov15a/sketch_nov15a.ino
+++ sketch_nov15a/sketch_nov15a.ino
@@ -1,4 +1,9 @@
+const int BUTTON_PIN = 11;
+
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting up...");
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   int pin;
   for (pin = 4; pin <= 10; pin++) {
     pinMode(pin, OUTPUT);
@@ -6,13 +11,35 @@ void setup() {
   }
 }
 
-int newstate = LOW;
 
-void loop() {
+
+int blinkstate = LOW;
+
+void blinking_8() {
   int pin;
   for (pin = 4; pin <= 10; pin++) {
-    digitalWrite(pin, newstate);
+    digitalWrite(pin, blinkstate);
   }
-  newstate ^= LOW ^ HIGH;
+  blinkstate ^= LOW ^ HIGH;
   delay(500);
+}
+
+int test_pin = 4;
+void test_segments() {
+  Serial.print("Turning on I/O pin: ");
+  Serial.println(test_pin);
+  // turn on the segment
+  digitalWrite(test_pin, LOW);
+  // now wait for LOW (button press)
+  while(digitalRead(BUTTON_PIN) == HIGH) delay(1);
+  // mow wait for HIGH (button releas)
+  while (digitalRead(BUTTON_PIN) == LOW) delay(1);
+  // turn off the segment
+  digitalWrite(test_pin, HIGH);
+  if (++test_pin > 10) test_pin = 4;
+}
+
+void loop() {
+   // blinking_8();
+   test_segments();
 }
